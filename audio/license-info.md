76477d6108227c8d197012014a505470642e7e42
Zachary A. Taylor <ztaylor45685@gmail.com>
2021-05-03 18:02:49 -0400

    Refactored alarm.js code and moved alarm.mp3 audio file
    alarm.js:
    - Changed variable naming conventions from snake case to camel case. I am a little
    too comfortable with Python variable naming conventions, it seems.
    - Added semicolons at the end of all statements
    
    alarm.mp3
    - Moved alarm.mp3 to audio/ directory and gave it a more descriptive name. This will
    set the stage for easily adding different types of alarm sounds in the future.
    
    license-info.md:
    - This file will include licensing information for any audio files used in the project.

---
+++ audio/license-info.md
@@ -0,0 +1,6 @@
+This file contains information about the license(s) associated with the audio files used
+in this project.
+
+| Audio File | Source | License |
+|------------|--------|---------|
+| [double-beep-beep-alarm.mp3](audio/double-beep-beep-alarm.mp3) | [salamisound](https://www.salamisound.com) | [License](https://www.salamisound.com/info1) |
--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-const SECONDS_PER_MINUTE = 60
-const SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60
-const MILLSECONDS_PER_SECOND = 1000
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
+const MILLSECONDS_PER_SECOND = 1000;
 
-document.addEventListener("DOMContentLoaded", setup)
+document.addEventListener("DOMContentLoaded", setup);
 
-var alarmAudio = new Audio('alarm.mp3');
+var alarmAudio = new Audio('audio/double-beep-beep-alarm.mp3');
 alarmAudio.loop = true;
 
 function setup(event) {
     // Setup Local storage with Pomodoro counter
-    var npc = localStorage.getItem('num-pomodoros-completed')
+    var npc = localStorage.getItem('num-pomodoros-completed');
     if(npc == null){
         //initialization
-        localStorage.setItem('num-pomodoros-completed', '0')
-        npc = 0
+        localStorage.setItem('num-pomodoros-completed', '0');
+        npc = 0;
     } else {
-        npc = Number.parseInt(npc)
+        npc = Number.parseInt(npc);
     }
 
-    var pomodoro_div = document.getElementById('pomodoro-count')
-    pomodoro_div.innerText = npc
+    var pomodoroDiv = document.getElementById('pomodoro-count');
+    pomodoroDiv.innerText = npc;
 
     // Set up event listeners for buttons
-    var start_button = document.getElementById('start-stop-button')
-    start_button.addEventListener('click', start_stop)
+    var startButton = document.getElementById('start-stop-button');
+    startButton.addEventListener('click', startStop);
 
-    var reset_button = document.getElementById('reset-button')
-    reset_button.addEventListener('click', reset)
+    var resetButton = document.getElementById('reset-button');
+    resetButton.addEventListener('click', reset);
 
     // setting up inputs:
     // TODO: figure out a way to make it so that whenever you type in characters,
-    // you they "backfill" from right to left, with there still being a placeholder of 0
+    // they "backfill" from right to left, with there still being a placeholder of 0
     // for a value that hasn't been completely filled in yet.
     // var seconds_input = document.getElementById('time-input-seconds')
     // var minutes_input = document.getElementById('time-input-minutes')
@@ -38,174 +38,173 @@ function setup(event) {
 }
 
 var TimerState = {
-    total_seconds: undefined,
-    timer_stopped: false
-}
+    totalSeconds: undefined,
+    timerStopped: false
+};
 
 // Changes a "Start" button into a "Stop" button
-function start_to_stop(start_stop_button) {
+function startToStop(StartStopButton) {
     // change inner text to "Stop"
-    start_stop_button.innerText = "Stop"
-    start_stop_button.classList.remove("start")
-    start_stop_button.classList.add("stop")
+    StartStopButton.innerText = "Stop";
+    StartStopButton.classList.remove("start");
+    StartStopButton.classList.add("stop");
 }
 
 // Changes a "Stop" button into a "Start" button
-function stop_to_start(start_stop_button) {
+function stopToStart(StartStopButton) {
     // change inner text to "Start"
-    start_stop_button.innerText = "Start"
-    start_stop_button.classList.remove("stop")
-    start_stop_button.classList.add("start")
-    alarmAudio.pause()
+    StartStopButton.innerText = "Start";
+    StartStopButton.classList.remove("stop");
+    StartStopButton.classList.add("start");
+    alarmAudio.pause();
 }
 
 // Changes to perform whenever a user clicks on the Start/Stop button
-function start_stop(event) {
-    var start_stop_button = event.target
-    if(start_stop_button.innerText == "Start") {
+function startStop(event) {
+    var startStopButton = event.target;
+    if(startStopButton.innerText == "Start") {
         // change inner text to "Stop"
-        start_to_stop(start_stop_button)
-        start_timer()
+        startToStop(startStopButton);
+        startTimer();
     } else {
         // change inner text to "Start"
-        stop_to_start(start_stop_button)
-        stop_timer()
+        stopToStart(startStopButton);
+        stopTimer();
     }
 }
 
 // Start or resume the timer
-function start_timer() {
+function startTimer() {
     // Get the values of current input elements, 
     // and convert into a time
-    var seconds_input = document.getElementById('time-input-seconds')
-    var seconds = Number.parseInt(seconds_input.value)
+    var secondsInput = document.getElementById('time-input-seconds');
+    var seconds = Number.parseInt(secondsInput.value);
 
-    var minutes_input = document.getElementById('time-input-minutes')
-    var minutes = Number.parseInt(minutes_input.value)
+    var minutesInput = document.getElementById('time-input-minutes');
+    var minutes = Number.parseInt(minutesInput.value);
 
-    var hours_input = document.getElementById('time-input-hours')
-    var hours = Number.parseInt(hours_input.value)
+    var hoursInput = document.getElementById('time-input-hours');
+    var hours = Number.parseInt(hoursInput.value);
 
     // Exit early if no time was specified
     if(hours == 0 && minutes == 0 && seconds == 0) {
-        reset()
-        return
+        reset();
+        return;
     }
 
     // Time-related code here
-    var total_seconds = undefined
-    TimerState.timer_stopped = false
-    if(TimerState.total_seconds === undefined) {
-        total_seconds = Number.parseInt(hours * SECONDS_PER_HOUR + minutes * SECONDS_PER_MINUTE + seconds)
+    var totalSeconds = undefined;
+    TimerState.timerStopped = false;
+    if(TimerState.totalSeconds === undefined) {
+        totalSeconds = Number.parseInt(hours * SECONDS_PER_HOUR + minutes * SECONDS_PER_MINUTE + seconds);
     } else {
-        total_seconds = TimerState.total_seconds
+        totalSeconds = TimerState.totalSeconds;
     }
     // TODO: Consider changing times to a single numerical value
-    var times = []
-    var timer_id = setInterval(() => {
+    var times = [];
+    var timerId = setInterval(() => {
         // timer has counted all the way down
         if(times.length > 0 && times[times.length - 1] <= 0) {
-            clearInterval(timer_id)
-            console.log('Done!')
-            countdown_complete()
-        } else if(TimerState.timer_stopped) {
-            console.log('Stopped!')
-            clearInterval(timer_id)
+            clearInterval(timerId);
+            console.log('Done!');
+            countdown_complete();
+        } else if(TimerState.timerStopped) {
+            console.log('Stopped!');
+            clearInterval(timerId);
         } else {
-            values = tick(hours_input, minutes_input, seconds_input, total_seconds, times)
-            hours_input = values[0]
-            minutes_input = values[1]
-            seconds_input = values[2]
-            total_seconds = values[3]
-            times = values[4]
+            values = tick(hoursInput, minutesInput, secondsInput, totalSeconds, times);
+            hoursInput = values[0];
+            minutesInput = values[1];
+            secondsInput = values[2];
+            totalSeconds = values[3];
+            times = values[4];
         }
-    }, MILLSECONDS_PER_SECOND)
+    }, MILLSECONDS_PER_SECOND);
 }
 
 // Function that computes one tick of the timer clock
-function tick(h_input, m_input, s_input, total_seconds, times){
-    total_remaining_seconds = total_seconds - 1
-    if(total_remaining_seconds == 0) {
-        h_input.value = '00'
-        m_input.value = '00'
-        s_input.value = '00'
-    } else if(total_remaining_seconds > 0) {
-        hours = Math.max(Math.floor(total_remaining_seconds / SECONDS_PER_HOUR), 0)
-        minutes = Math.max(Math.floor((total_remaining_seconds - (hours * SECONDS_PER_HOUR)) / SECONDS_PER_MINUTE), 0)
-        seconds = total_remaining_seconds - ((hours * SECONDS_PER_HOUR) + (minutes * SECONDS_PER_MINUTE))
+function tick(hInput, mInput, sInput, totalSeconds, times){
+    var totalRemainingSeconds = totalSeconds - 1;
+    if(totalRemainingSeconds == 0) {
+        hInput.value = '00';
+        mInput.value = '00';
+        sInput.value = '00';
+    } else if(totalRemainingSeconds > 0) {
+        hours = Math.max(Math.floor(totalRemainingSeconds / SECONDS_PER_HOUR), 0);
+        minutes = Math.max(Math.floor((totalRemainingSeconds - (hours * SECONDS_PER_HOUR)) / SECONDS_PER_MINUTE), 0);
+        seconds = totalRemainingSeconds - ((hours * SECONDS_PER_HOUR) + (minutes * SECONDS_PER_MINUTE));
         if(hours == 0) {
-            h_input.value = '00'
+            hInput.value = '00';
         } else {
             if(hours < 10) {
-                h_input.value = '0' + hours
+                hInput.value = '0' + hours;
             } else {
-                h_input.value = hours
+                hInput.value = hours;
             }
         }
 
         if(minutes == 0) {
-            m_input.value = '00'
+            mInput.value = '00';
         } else {
             if(minutes < 10) {
-                m_input.value = '0' + minutes
+                mInput.value = '0' + minutes;
             } else {
-                m_input.value = minutes
+                mInput.value = minutes;
             }
         }
 
         if(seconds == 0) {
-            s_input.value = '00'
+            sInput.value = '00';
         } else {
             if(seconds < 10) {
-                s_input.value = '0' + seconds
+                sInput.value = '0' + seconds;
             } else {
-                s_input.value = seconds
+                sInput.value = seconds;
             }
         }
     }
-    times.push(total_remaining_seconds)
-    return [h_input, m_input, s_input, total_remaining_seconds, times]
+    times.push(totalRemainingSeconds);
+    return [hInput, mInput, sInput, totalRemainingSeconds, times];
 }
 
 // Stops the timer
-function stop_timer() {
-    TimerState.timer_stopped = true
+function stopTimer() {
+    TimerState.timerStopped = true;
 }
 
 // Updates Pomodoro count based on current mode
 function countdown_complete() {
-    // TODO: maybe add some sound effects here?
-
-    alarmAudio.play();   
+    // Play sound
+    alarmAudio.play();
     // Update Pomodoro count based on current state
-    work_or_play_input = document.getElementById('work-or-play-mode')
+    work_or_play_input = document.getElementById('work-or-play-mode');
     // If it's checked, we're working. If not, we're playing :)
-    var pomodoro_div = document.getElementById('pomodoro-count')
-    var pomodoro_count = Number.parseInt(pomodoro_div.innerText)
+    var pomodoroDiv = document.getElementById('pomodoro-count');
+    var pomodoro_count = Number.parseInt(pomodoroDiv.innerText);
     if(work_or_play_input.checked) {
-        pomodoro_div.innerText = pomodoro_count + 1
+        pomodoroDiv.innerText = pomodoro_count + 1;
     } else {
-        pomodoro_div.innerText = pomodoro_count - 1
+        pomodoroDiv.innerText = pomodoro_count - 1;
     }
-    localStorage.setItem('num-pomodoros-completed', pomodoro_div.innerText)
+    localStorage.setItem('num-pomodoros-completed', pomodoroDiv.innerText);
 }
 
 // Reset the timer to the default state, but don't clear the Pomodoro count
 function reset(event){
-    stop_timer()
-    TimerState.total_seconds = undefined
+    stopTimer();
+    TimerState.totalSeconds = undefined;
 
-    var start_stop_button = document.getElementById('start-stop-button')
-    stop_to_start(start_stop_button)
+    var start_stop_button = document.getElementById('start-stop-button');
+    stopToStart(start_stop_button);
 
-    var seconds_input = document.getElementById('time-input-seconds')
-    var minutes_input = document.getElementById('time-input-minutes')
-    var hours_input = document.getElementById('time-input-hours')
+    var secondsInput = document.getElementById('time-input-seconds');
+    var minutesInput = document.getElementById('time-input-minutes');
+    var hoursInput = document.getElementById('time-input-hours');
 
-    seconds_input.placeholder = '00'
-    seconds_input.value = '00'
-    minutes_input.placeholder = '00'
-    minutes_input.value = '00'
-    hours_input.placeholder = '00'
-    hours_input.value = '00'
+    secondsInput.placeholder = '00';
+    secondsInput.value = '00';
+    minutesInput.placeholder = '00';
+    minutesInput.value = '00';
+    hoursInput.placeholder = '00';
+    hoursInput.value = '00';
 }
